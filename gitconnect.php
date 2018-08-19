0c51da2d9d1f1a645418cf0d335c783384bbb832
Thomas Pickup <hello@thomaspickup.co.uk>
2018-08-19 14:58:52 +0200

    Basic Widget Layout
    Layout created to print Hello World.

--- gitconnect.php
+++ gitconnect.php
@@ -12,3 +12,65 @@ Version: 0.1
 Author URI: https://thomaspickup.co.uk
 Text Domain: gitconnect
 */
+
+// Register and load the widget
+function gitconnect_load() {
+    register_widget( 'gitconnect_widget' );
+}
+
+add_action( 'widgets_init', 'gitconnect_load' );
+ 
+// Creating the widget 
+class gitconnect_widget extends WP_Widget {
+    function __construct() {
+        parent::__construct(
+
+        // Base ID of your widget
+        'gitconnect', 
+
+        // Widget name will appear in UI
+        __('gitconnect', 'gitconnect_domain'), 
+
+        // Widget description
+        array( 'description' => __( 'A link between GitHub and WordPress.', 'gitconnect_domain' ), ) 
+        );
+    }
+ 
+    // Creating widget front-end
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+
+        // before and after widget arguments are defined by themes
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+        echo $args['before_title'] . $title . $args['after_title'];
+
+        // This is where you run the code and display the output
+        echo __( 'Hello, World!', 'gitconnect_domain' );
+        echo $args['after_widget'];
+    }
+         
+    // Widget Backend 
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+        $title = $instance[ 'title' ];
+        }
+        else {
+        $title = __( 'New title', 'gitconnect_domain' );
+        }
+        // Widget admin form
+        ?>
+        <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+        <?php 
+    }
+
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+}
