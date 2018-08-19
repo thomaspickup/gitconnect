029e67d0cb62e6916219996a477d56f4f0bf46b0
Thomas Pickup <hello@thomaspickup.co.uk>
2018-08-19 14:15:59 +0200

    Initial plugin files
    Created the plugin PHP page and the readme.txt file

---
+++ gitconnect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package gitconnect
+ * @version 0.1
+ */
+/*
+Plugin Name: gitconnect
+Plugin URI: https://github.com/thomaspickup/gitconnect
+Description: GitConnect links a WordPress site with GitHub. When activated this plugin will embed your most recent GitHub repositories on a WordPress page as well as the Language, Description, Last Updated time, and of course a link to that repository.
+Author: Thomas Pickup
+Version: 0.1
+Author URI: https://thomaspickup.co.uk
+Text Domain: gitconnect
+*/
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== gitconnect ===
+Contributors: Thomas
+Stable tag: 0.1
+Tested up to: 4.9
+Requires at least: 4.9
+
+GitConnect links a WordPress site with GitHub. 
+
+== Description ==
+
+GitConnect links a WordPress site with GitHub. When activated this plugin will embed your most recent GitHub repositories on a WordPress page as well as the Language, Description, Last Updated time, and of course a link to that repository.
